e8ee8f971547d70b40aea0ffd4f36e1fe79e76c6
viktoras <viktoras@eli5.io>
2022-10-29 12:39:20 +0300

    table names

--- warehouse/models.py
+++ warehouse/models.py
@@ -7,13 +7,22 @@ class Component(models.Model):
     characteristics = models.CharField(max_length=100)
     quantity = models.IntegerField
 
+    class Meta:
+        db_table = 'components'
+
 
 class User(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=32)
     email = models.EmailField()
 
+    class Meta:
+        db_table = 'users'
+
 
 class UserComponentPivot(models.Model):
     component_id = models.ForeignKey(Component, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'components_users_pivot'
